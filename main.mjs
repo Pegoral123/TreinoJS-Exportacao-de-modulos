5b3638f34621d62d59105d7e68e41e457bc4901f
Pegoral123 <jhonathansilva2123@gmail.com>
2022-06-13 18:10:53 -0300

    salvando

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Jhonathan', 20));
+
+console.log(mostraCidade('pegoral', 'São Paulo'));
+
+console.log(mostraHobby('jogar', 'Ouvir música'));
